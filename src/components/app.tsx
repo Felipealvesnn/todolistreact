a7060ff10f83fac25561d9459e6b71c62aa2e91d
Felipe Nunes Work <felipe.rudra@hotmail.com>
2023-12-13 14:32:53 -0300

    att

---
+++ src/components/app.tsx
@@ -0,0 +1,21 @@
+const App = () => {
+
+    const estilo = {
+      height: "100vh",
+      alignItems: "center",
+      display: "flex",
+      justifyContent: "center",
+      justifyItems: "center"
+    };
+  
+    return (
+      <>
+        <div style={estilo}>
+          <h1>Todo List</h1>
+        </div>
+      </>
+    );
+  }
+  
+  export default App;
+  
