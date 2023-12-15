f2243ea124f8af6028568010fb15271810dd3d9b
Felipe Nunes Work <felipe.rudra@hotmail.com>
2023-12-15 16:13:30 -0300

    ATT

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,57 +1,76 @@
-
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import UIkit from "uikit";
 import { Todo } from "../models/Todo";
 import { TodoContextType } from "../contexts/TodoContextType";
+
+import EditModal from "./EditModal"; // Importe ou crie um componente para o modal de edição
 import { TodoContext } from "../contexts/todoContex";
 
 interface TodoListItemProps {
-
-    taks: Todo;
-    
-
+    task: Todo;
 }
 
-
-const TodoListemItem = (props: TodoListItemProps) => {
-    const {  addTodo, removeTodo, toggle } = useContext<TodoContextType>(TodoContext);
+const TodoListItem = (props: TodoListItemProps) => {
+    const { removeTodo, toggle } = useContext<TodoContextType>(TodoContext);
+    const [isEditing, setIsEditing] = useState(false);
 
     const onRemove = (todo: Todo) => {
         removeTodo(todo);
+    };
 
-    }
     const handleChange = () => {
-        toggle(props.taks);
+        toggle({ ...props.task, done: !props.task.done });
+    };
 
-    }
+    const handleEditClick = () => {
+        setIsEditing(true);
+    };
+
+    const closeModal = () => {
+        setIsEditing(false);
+    };
+
     return (
         <>
-            <tr className="uk-animation-slide-bottom-medium ">
+            <tr className="uk-animation-slide-bottom-medium">
                 <td>
                     <label>
-                        <input className="uk-checkbox"
+                        <input
+                            className="uk-checkbox"
                             type="checkbox"
-                            onChange={handleChange
-                            }
-                            checked={props.taks.done} />
+                            onChange={handleChange}
+                            checked={props.task.done}
+                        />
                     </label>
                 </td>
-                <td className="uk-width-auto">
-                    {props.taks.task}</td>
-                <td className="uk-width-auto">
-                    {props.taks.description}</td>
+                <td className="uk-width-auto">{props.task.task}</td>
+                <td className="uk-width-auto">{props.task.description}</td>
 
                 <td className="uk-width-auto">
-
-                    <button uk-icon="trash" onClick={() => onRemove(props.taks)}
-                        className="uk-button uk-button-danger">Delete
+                    <button
+                        uk-icon="trash"
+                        onClick={() => onRemove(props.task)}
+                        className="uk-button uk-button-danger"
+                    >
+                        Delete
                     </button>
-                    <button className="uk-button uk-button-primary">Edit
+                    <button
+                        className="uk-button uk-button-primary"
+                        onClick={handleEditClick}
+                    >
+                        Edit
                     </button>
                 </td>
-
             </tr>
-        </>
-    )
 
-}
-export default TodoListemItem;
+            {isEditing && (
+                <EditModal
+                    task={props.task}
+                    closeModal={closeModal}
+                />
+            )}
+        </>
+    );
+};
+
+export default TodoListItem;
--- src/contexts/todoContex.tsx
+++ src/contexts/todoContex.tsx
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+// Importe os tipos necessários e o useState, se ainda não estiverem importados
+import React, { createContext, useState } from 'react';
 import { TodoContextType } from "./TodoContextType";
 import { Todo } from "../models/Todo";
 
@@ -7,38 +8,62 @@ export const TodoContext = createContext<TodoContextType>({
     addTodo: () => { },
     removeTodo: () => { },
     toggle: () => { }
-
-})
+});
 
 const TodoProvider = (props: any) => {
-    const todo: Todo[] = [{
-        id: 1,
-        task: "Task 1",
-        description: "Description 1",
-        done: false
-    },
-    {
-        id: 2,
-        task: "Task 2",
-        description: "Description 2",
-        done: true
-    },]
-    const addTodo = (task: string, description: string) => { }
-    const removeTodo = (todo: Todo) => { }
-    const toggle = (todo: Todo) => { }
+    // Estado para armazenar a lista de tarefas
+    const [todos, setTodos] = useState<Todo[]>([
+        {
+            id: 1,
+            task: "Task 1",
+            description: "Description 1",
+            done: false
+        },
+        {
+            id: 2,
+            task: "Task 2",
+            description: "Description 2",
+            done: true
+        }
+    ]);
+
+    // Método para adicionar uma nova tarefa
+    const addTodo = (task: string, description: string) => {
+        const newTodo: Todo = {
+            id: todos.length + 1,
+            task,
+            description,
+            done: false
+        };
+
+        setTodos([...todos, newTodo]);
+    };
+
+    // Método para remover uma tarefa
+    const removeTodo = (todo: Todo) => {
+        const updatedTodos = todos.filter(t => t.id !== todo.id);
+        setTodos(updatedTodos);
+    };
+
+    // Método para editar uma tarefa
+    const toggle = (editedTodo: Todo) => {
+        const updatedTodos = todos.map(t =>
+            t.id === editedTodo.id ? editedTodo : t
+        );
+
+        setTodos(updatedTodos);
+    };
 
     return (
         <TodoContext.Provider value={{
-            todo,
+            todo: todos,
             addTodo,
             removeTodo,
             toggle
-        }}
-        >
+        }}>
             {props.children}
         </TodoContext.Provider>
-    )
-
-}
+    );
+};
 
 export default TodoProvider;
