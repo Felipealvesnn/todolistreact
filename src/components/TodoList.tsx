d4c991a2903bc2fde9643df5980f24e5591d2840
Felipe Nunes Work <felipe.rudra@hotmail.com>
2023-12-15 15:28:22 -0300

    att

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,20 +6,9 @@ import TodoListemItem from "./TodoListItem";
 const TodoList: React.FC = () => {
 
 
-    const Todosrs: Todo[] = [{
-        id: 1,
-        task: "Task 1",
-        description: "Description 1",
-        done: false
-    },
-    {
-        id: 2,
-        task: "Task 2",
-        description: "Description 2",
-        done: true
-    },
+    const Todosrs: Todo[] = [
+    
     ];
-    const [todos, setTodos] = useState(Todosrs);
     return (
         <>
             <table className="uk-table uk-table-striped">
@@ -34,7 +23,7 @@ const TodoList: React.FC = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {todos?.map((todo) => (
+                    {Todosrs?.map((todo) => (
                         <TodoListemItem key={todo.id} taks={todo} />
                     ))}
                 </tbody>
--- src/contexts/todoContex.tsx
+++ src/contexts/todoContex.tsx
@@ -1,5 +1,6 @@
 import { createContext } from "react";
 import { TodoContextType } from "./TodoContextType";
+import { Todo } from "../models/Todo";
 
 export const TodoContext = createContext<TodoContextType>({
     todo: [],
@@ -8,3 +9,37 @@ export const TodoContext = createContext<TodoContextType>({
     toggle: () => { }
 
 })
+
+const todoProvider = (props: any) => {
+    const todo: Todo[] = [{
+        id: 1,
+        task: "Task 1",
+        description: "Description 1",
+        done: false
+    },
+    {
+        id: 2,
+        task: "Task 2",
+        description: "Description 2",
+        done: true
+    },]
+    const addTodo = (task: string, description: string) => { }
+    const removeTodo = (todo: Todo) => { }
+    const toggle = (todo: Todo) => { }
+
+    return (
+        <TodoContext.Provider value={{
+            todo,
+            addTodo,
+            removeTodo,
+            toggle
+        }}
+
+        >
+            {props.children}
+        </TodoContext.Provider>
+    )
+
+}
+
+export default todoProvider;
