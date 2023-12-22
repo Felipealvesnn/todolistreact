810509f10adf453792c4665edbaf1bbc82e17713
Felipe Nunes Work <felipe.rudra@hotmail.com>
2023-12-22 13:34:35 -0300

    att

--- src/contexts/TodoContextType.tsx
+++
@@ -1,8 +0,0 @@
-import { Todo } from "../models/Todo";
-
-export interface TodoContextType {
-    todo: Todo[];
-    addTodo: (task: string, description: string) => void;
-    removeTodo: (todo: Todo) => void;
-    toggle: (todo: Todo) => void;
-}
